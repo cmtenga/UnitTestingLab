0f70fc9e4fb0fe82ef8c0b3e3dc4d27096e1b09c
Owner <Owner@Owner-PC.gateway.2wire.net>
2012-04-02 21:40:03 -0500

    More test classes added

---
+++ test/common/InvoiceTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
+import java.util.Date;
+import java.util.List;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Owner
+ */
+public class InvoiceTest {
+
+    public InvoiceTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
---
+++ test/common/LineItemTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package common;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Owner
+ */
+public class LineItemTest {
+
+    public LineItemTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
